451bd7e825925b72fc0eb7734ebfe7ae54c7182b
zhouxiaoqi13 <zhouxiaoqi1@jd.com>
2022-08-18 00:49:14 +0800

    feat: add tool window demo

---
+++ src/main/java/org/chromie/view/ChromieBoardWindowFactory.java
@@ -0,0 +1,28 @@
+package org.chromie.view;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author zhouxiaoqi1
+ * @date 2022/8/17 23:47
+ */
+public class ChromieBoardWindowFactory implements ToolWindowFactory {
+    /**
+     * Create the tool window content.
+     *
+     * @param project    current project
+     * @param toolWindow current tool window
+     */
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        ChromieWindow myToolWindow = new ChromieWindow(toolWindow);
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        Content content = contentFactory.createContent(myToolWindow.getContent(), "", false);
+        toolWindow.getContentManager().addContent(content);
+    }
+}
