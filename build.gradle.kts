ac692a4b226f7b8a9f8a63c7ba03f4ce17564c55
liushuai53 <liushuai7@jd.com>
2022-08-19 13:24:00 +0800

    dev(Chromie):版本调整

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 
 // Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
 intellij {
-    version.set("2020.1")
+    version.set("2021.2")
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf(/* Plugin Dependencies */))
@@ -32,7 +32,7 @@ tasks {
     }
 
     patchPluginXml {
-        sinceBuild.set("201")
+        sinceBuild.set("202")
         untilBuild.set("222.*")
     }
 
